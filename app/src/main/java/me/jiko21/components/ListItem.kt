c0a1a5cd5c70651d2e731f473f9ca02a7b9a4330
jiko21 <daikikojima0531@icloud.com>
2022-01-15 00:17:44 +0900

    update: style of task lists

--- app/src/main/java/me/jiko21/components/ListItem.kt
+++ app/src/main/java/me/jiko21/components/ListItem.kt
@@ -1,16 +1,39 @@
 package me.jiko21.components
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.jiko21.R
 import me.jiko21.entity.Task
 
 @Composable
 fun ListItem(task: Task) {
-    Row {
-       Text(text = task.content)
+    val padding = 16.dp
+    Card(
+        backgroundColor = colorResource(R.color.background),
+        modifier = Modifier
+            .border(
+                width = 0.dp,
+                color = Color.Transparent,
+                shape = RoundedCornerShape(10.dp),
+            )
+            .fillMaxWidth()
+    ) {
+        Row(Modifier.padding(padding)) {
+            Text(text = task.content, fontSize = 24.sp)
+        }
     }
 }
 
--- app/src/main/java/me/jiko21/components/TaskLists.kt
+++ app/src/main/java/me/jiko21/components/TaskLists.kt
@@ -1,14 +1,17 @@
 package me.jiko21.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.jiko21.entity.Task
 
 @Composable
 fun TaskLists(lists: List<Task>) {
-    LazyColumn {
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(4.dp),) {
         items(lists) { task ->
             ListItem(task = task)
         }
