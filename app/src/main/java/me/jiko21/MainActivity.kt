3270eb7b5028202100ee3ef99f37437629f9418c
jiko21 <daikikojima0531@icloud.com>
2022-01-16 23:09:03 +0900

    update: connect to view model

--- app/src/main/java/me/jiko21/MainActivity.kt
+++ app/src/main/java/me/jiko21/MainActivity.kt
@@ -3,6 +3,7 @@ package me.jiko21
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,47 +15,45 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
 import me.jiko21.components.TaskCreateDialog
 import me.jiko21.components.TaskLists
 import me.jiko21.entity.Task
 import me.jiko21.ui.theme.TodoComposeTheme
+import me.jiko21.viewmodel.TaskViewModel
+import me.jiko21.viewmodel.TaskViewModelFactory
 
 class MainActivity : ComponentActivity() {
+    private val taskViewModel: TaskViewModel by viewModels {
+        TaskViewModelFactory((application as TaskApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             TodoComposeTheme {
-                Container()
+                Container(taskViewModel)
             }
         }
     }
 }
 
 @Composable
-fun Container() {
-    val lists = arrayListOf(
-        Task(1, "test1"),
-        Task(2, "test2"),
-        Task(3, "test3"),
-        Task(4, "test4"),
-        Task(5, "test5"),
-        Task(6, "test6"),
-        Task(7, "test7"),
-        Task(8, "test8"),
-        Task(9, "test9"),
-        Task(10, "test10"),
-        Task(11, "test11"),
-        Task(12, "test12")
-    )
+fun Container(viewModel: TaskViewModel) {
 
     var isCreateModalOpen by remember {
         mutableStateOf(false)
     }
+
+    val lists = viewModel.tasks.observeAsState(arrayListOf())
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
@@ -68,10 +67,17 @@ fun Container() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colors.background
             ) {
-                TaskLists(lists = lists)
+                lists.value?.let {
+                    TaskLists(lists = lists.value, onDelete = {id ->
+                        viewModel.remove(id)
+                    })
+                }
                 TaskCreateDialog(isCreateModalOpen,
                     onDismiss = { isCreateModalOpen = false },
-                    onSubmit = { isCreateModalOpen = false },
+                    onSubmit = { task ->
+                        isCreateModalOpen = false
+                        viewModel.insert(task)
+                    },
                     onCancel = { isCreateModalOpen = false },
                 )
             }
