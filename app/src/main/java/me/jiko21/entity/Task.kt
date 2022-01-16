0c313f07eec5c1909496f7fee17c4f67e94a0835
jiko21 <daikikojima0531@icloud.com>
2022-01-16 22:45:27 +0900

    update: make id nullable

--- app/src/main/java/me/jiko21/entity/Task.kt
+++ app/src/main/java/me/jiko21/entity/Task.kt
@@ -1,3 +1,13 @@
 package me.jiko21.entity
 
-data class Task(val id: Long, val content: String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task")
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+    @ColumnInfo(name = "content")
+    val content: String
+)
